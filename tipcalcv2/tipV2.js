ba0848c5d3ec3c2e4a62997307af647840ee1595
Chitru Shrestha <schitru@gmail.com>
2016-07-21 23:04:24 +0545

    tipCalcv2 completed

--- tipcalcv2/tipV2.js
+++ tipcalcv2/tipV2.js
@@ -1,6 +1,9 @@
+$('document').ready(function(){
+
+
 function calcTip() {
-	var subtotalElem = document.getElementById('subtotal');
 	var tipElem = document.getElementById('tip');
+	var subtotalElem = document.getElementById('subtotal');
 	var totalElem = document.getElementById('total');
 	var subtotal = parseFloat(subtotalElem.value);
 	var tip = parseFloat(tipElem.value);
@@ -8,3 +11,10 @@ function calcTip() {
 	totalElem.innerHTML = '$' + total;
 
 }
+
+var btn = document.getElementById('btn');
+btn.onclick = calcTip;
+
+
+
+});
